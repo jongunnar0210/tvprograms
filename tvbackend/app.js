77958509186ad24050b9c483f58c2421cdba54e4
Jón Gunnar Þórarinsson <jongunnar0210@gmail.com>
2020-12-12 14:19:35 +0000

    More stuff works

--- tvbackend/app.js
+++ tvbackend/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 let request = require('request');
+const moment = require('moment');
 const app = express();
 const port = 5000;
+const DATE_FORMAT = 'YYYY-MM-DD';
 
 app.get('/channels', (req, res) => {
+  console.log('Was asked for channels');
+
   request('https://api.stod2.is/dagskra/api', function(error, response, body) {
     if (!error && response.statusCode === 200) {
+      console.log('Got channels');
       res.send(body);
+    } else {
+      console.log(`Error getting channels: ${error}`);
     }
   });
 });
@@ -15,11 +22,14 @@ app.get('/programs/:channel/:date', (req, res) => {
   let channel = req.params.channel;
   let date = req.params.date;
 
+  console.log(`Was asked for programs. Channel: ${channel}. Date: ${date}`);
+
   request(`https://api.stod2.is/dagskra/api/${channel}`, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      res.send(body);
+      console.log('Got programs');
+      res.send(constructProgramList(JSON.parse(body), date));
     } else {
-      res.send('Error code ' + response.statusCode);
+      res.send('Error code ' + error);
     }
   });
 });
@@ -27,3 +37,25 @@ app.get('/programs/:channel/:date', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
+
+function constructProgramList(programs, date) {
+  let retval = [];
+
+  for (const program of programs) {
+    const programDate = moment(program.dagsetning).format(DATE_FORMAT);
+
+    if (programDate === date) {
+      // Include this program since it has the date we are asked for:
+      const item = {
+        dagsetning: date,
+        isltitill: program.isltitill,
+        upphaf: program.upphaf,
+        thattur: program.thattur,
+        thattafjoldi: program.thattafjoldi
+      };
+      retval.push(item);
+    }
+  }
+
+  return retval;
+}
--- tvfrontend/src/components/MainPage.js
+++ tvfrontend/src/components/MainPage.js
@@ -15,7 +15,7 @@ export default function MainPage() {
   const DATE_FORMAT = 'YYYY-MM-DD';
 
   useEffect(() => {
-    // Populate the date dropdown list:
+    // Populate the date dropdown list of dates from yesterday to one week into the future:
     let theDates = [];
     let aDate = new Date();
     aDate.setDate(aDate.getDate() - 1);
@@ -26,37 +26,70 @@ export default function MainPage() {
     }
     set_dates(theDates);
 
+    // Set the selected date to today:
+    const selInitialDate = theDates[1];
+    set_selectedDate(selInitialDate);
+
+    console.log('Getting the channels...');
     // Populate the channel dropdown list:
     axios.get('/channels').then(response => {
       let theChannels = response.data;
       theChannels.push("RÚV");  // Add our extra channel, RÚV.
       set_channels(response.data);
-    });
 
-    // Populate the main tv program list:
-    axios.get('/programs/stod2/2020-12-10').then(response => {
-      //console.log(response.data[1].isltitill);
-      set_programs(response.data);
+      // Set the selected channel:
+      if (theChannels.length > 0) {
+        const selChannel = theChannels[0];
+        set_selectedChannel(selChannel);
+
+        // Populate the main tv program list:
+        axios.get(`/programs/${selChannel}/${selInitialDate}`).then(response => {
+          console.log(JSON.stringify(response));
+          set_programs(response.data);
+        });
+      }
     });
   }, []);
+
+  function changeDate(date) {
+    console.log('Changed date to ' + date);
+    set_selectedDate(date);
+
+    // Populate the main tv program list:
+    axios.get(`/programs/${selectedChannel}/${date}`).then(response => {
+      set_programs(response.data);
+    });
+  }
+
+  function changeChannel(channel) {
+    console.log('Changed channel to ' + channel + ' selectedDate: ' + selectedDate);
+    set_selectedChannel(channel);
+
+    // Populate the main tv program list:
+    axios.get(`/programs/${channel}/${selectedDate}`).then(response => {
+      set_programs(response.data);
+    });
+  }
 
   return (
     <Form className='mainPage'>
       <Form.Group as={Row} controlId='dropdownListsRow'>
         <Col sm={6}>
-          <Form.Control as='select'>
+          <Form.Control as='select' value={selectedDate} onChange={e => changeDate(e.target.value)}>
             {dates.map(d => <option key={d} value={d}>{d}</option>)}
           </Form.Control>        
         </Col>
         <Col sm={6}>
-          <Form.Control as='select'>
+          <Form.Control as='select' value={selectedChannel} onChange={e => changeChannel(e.target.value)}>
             {channels.map(c => <option key={c} value={c}>{c}</option>)}
           </Form.Control>
         </Col>
       </Form.Group>
       <Form.Group as={Row} controlId='programListRow'>
         <ListGroup>
-          {programs.map((p, index) => <ListGroup.Item key={index}>{p.isltitill} {p.upphaf}</ListGroup.Item>)}
+          {programs.map((p, index) => <ListGroup.Item key={index}>
+            {p.isltitill} {p.upphaf} {p.thattur} af {p.thattafjoldi}
+          </ListGroup.Item>)}
         </ListGroup>
       </Form.Group>
     </Form>
