804b955c654fbff3e423b4dc4ac79e25f4a0f6b6
Jón Gunnar Þórarinsson <jongunnar0210@gmail.com>
2020-12-14 01:12:04 +0000

    Header now has background image

--- tvfrontend/src/components/Header.js
+++ tvfrontend/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import '../App.css';
 import { Form, Image } from "react-bootstrap";
+import LogoImage from '../multicolor-holiday-christmas-decorations-with-blue-silver-ribbon-snowflakes-header-background-hd-1920x720.jpg';
+
+var sectionStyle = {
+   backgroundImage: `url(${LogoImage})`
+}
 
 export default function Header({program, index}) {
   return (
-    <header>
+    <header style={sectionStyle}>
       <ul>
         <li key='header1'>
           <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Vodafone_icon.svg/1020px-Vodafone_icon.svg.png' />
